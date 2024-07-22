7de8663de23964d427b18f6d8701209cceb08cd2
Aleksander <trimadgic@proton.me>
2024-07-23 01:38:00 +0300

    test

--- app/api/media-v2/[...param]/route.ts
+++ app/api/media-v2/[...param]/route.ts
@@ -1,7 +1,9 @@
+export const runtime = "edge"
+
 import { getRequestContext } from "@cloudflare/next-on-pages"
 
 export async function GET(request: Request, param: any) {
   const { env } = getRequestContext()
 
-  return Response.json(env)
+  return Response.json({})
 }
